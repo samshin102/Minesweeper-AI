0017aa81d52cbed7ee3f9b20c2b0951cf2afeab1
Раскольников <polarjarod@gmail.com>
2016-11-13 02:57:05 -0600

    work

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,11 @@ public class Main {
 	
 	public void init(){
 		MineSweeper thready = new MineSweeper();
+		Thread loopy = new Thread(thready);
+		loopy.start();
 	}
 	
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		Main MineSweeper = new Main();
 	}
