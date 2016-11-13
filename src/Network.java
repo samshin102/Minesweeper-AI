e3d75d57b488030335f0dedb25ad631af6f7c72d
champfish <cj.duffee@gmail.com>
2016-11-12 23:12:16 -0600

    Added Output stuff

--- src/Network.java
+++ src/Network.java
@@ -1,49 +1,76 @@
 import java.util.Random;
 
 public class Network {
-
-	final int inputDepth;
+	final float fitness = 0;
+	final int inputWidth;
 	final int hiddenWidth;
 	final int hiddenDepth;
-	final int outputDepth;
-	Neuron[][] hiddenLayer;
-	Neuron[] inputLayer;
-	Random rand = new Random();
+	final int outputWidth;
+	Neuron[][] net;
+	Random randy = new Random();
 
 	public Network() {
-		inputDepth = 9;
+		inputWidth = 9;
 		hiddenWidth = 3;
 		hiddenDepth = 3;
-		outputDepth = 1;
+		outputWidth = 1;
 		createNetwork();
 	}
 
 	public Network(int inputDepth, int hiddenWidth, int hiddenDepth, int outputDepth) {
-		this.inputDepth = inputDepth;
+		this.inputWidth = inputDepth;
 		this.hiddenWidth = hiddenWidth;
 		this.hiddenDepth = hiddenDepth;
-		this.outputDepth = outputDepth;
+		this.outputWidth = outputDepth;
 		createNetwork();
 	}
 
-	public void createNetwork() {
-		inputLayer = new Neuron[inputDepth];
-		
-		hiddenLayer = new Neuron[hiddenDepth][hiddenWidth];
-		for (int row = 0; row < hiddenLayer.length; row++) {
-			for (int col = 0; col < hiddenLayer[0].length; col++) {
-				hiddenLayer[row][col] = new Neuron(rand.nextFloat());
+	private void createNetwork() {
+		net = new Neuron[hiddenDepth + 1][0];
+		net[0] = new Neuron[inputWidth];
+		for (int r = 0; r < net.length - 1; r++) {
+			for (int c = 0; c < net[r].length; c++) {
+				net[r][c] = new Neuron(hiddenWidth);
 			}
+		}
+		for (int i = 0; i < net[net.length - 1].length; i++) {
+			net[net.length - 1][i] = new Neuron(outputWidth);
 		}
 	}
 
 	public void mutate() {
-
+		int r = randy.nextInt(hiddenWidth + 1);
+		int c = randy.nextInt(net[r].length);
+		net[r][c].mutate();
 	}
 
-	public float getOutput(float[] inputValues) {
+	public float[] getOutput(float[] inputValues) {
+		for (int i = 0; i < inputValues.length; i++) {
+			net[0][1].val = inputValues[i];
+		}
+		for (int r = 1; r < net.length - 1; r++) {
+			for (int c = 0; c < net[r].length; c++) {
+				for (int nc = 0; nc > net[r + 1].length; nc++) {
+					net[r + 1][nc].val += net[r][c].getValue(nc);
+				}
+			}
+		}
+		float[] output = new float[outputWidth];
+		for (int i = 0; i < net[net.length - 1].length; i++) {
+			for (int nc = 0; nc < outputWidth; nc++) {
+				output[nc] += net[net.length - 1][i].getValue(nc);
+			}
+		}
+		return output;
+	}
 
-		
-
+	public int compareTo(Network other) {
+		if(fitness==other.fitness) {
+			return 0;
+		}
+		if(fitness>other.fitness) {
+			return 1;
+		}
+		return -1;
 	}
 }
